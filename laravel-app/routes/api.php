d529553113820af6f80bff2bd213ec66940d6395
BuckyUnlimited <bunnat029@gmail.com>
2026-09-01 23:11:47 +0700

    complete session7.2

--- laravel-app/routes/api.php
+++ laravel-app/routes/api.php
@@ -48,7 +48,7 @@ Route::middleware(['auth:sanctum', 'enabled'])->group(function () {
         });
     });
 
-    Route::prefix('chats')->group(function () {
+Route::prefix('chats')->group(function () {
         Route::get('/', [ChatController::class, 'getChats']);
         Route::get('/users', [ChatController::class, 'getChatUsers']);
         // Chat creation and management
@@ -58,5 +58,9 @@ Route::middleware(['auth:sanctum', 'enabled'])->group(function () {
         Route::delete('/delete/{chatId}', [ChatController::class, 'deleteChat']);
         Route::put('/group/update/{chatId}', [ChatController::class, 'updateGroupChat']);
         Route::delete('/group/leave/{chatId}', [ChatController::class, 'leaveGroupChat']);
+
+        Route::get('/group/{chatId}/members', [ChatController::class, 'getGroupChatMembers']);
+        Route::post('/group/{chatId}/members/add', [ChatController::class, 'addGroupChatMember']);
+        Route::delete('/group/{chatId}/members/remove/{memberId}', [ChatController::class, 'removeGroupChatMember']);
     });
 });
